ddfa8e38358986386093e1923314e6bec5910f5b
anish-santim <107955181+anish-santim@users.noreply.github.com>
2024-01-18 20:41:37 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,18 @@
+import asyncio 
+import websockets
+
+async def hello():
+    url = "ws://15.207.111.252:8765"
+    async with websockets.connect(url) as websocket:
+        name = input("What is your name? ")
+
+        await websocket.send(name)
+
+        print("Client sent: ", name)
+
+        greeting = await websocket.recv()
+
+        print("Client received: ", greeting)
+
+if __name__ == "__main__":
+    asyncio.run(hello())
